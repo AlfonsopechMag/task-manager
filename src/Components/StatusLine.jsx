9d4f5d27c985171ef28679b4fe21edc7e45d7614
AlfonsopechMag <alfonsoisup@gmail.com>
2024-01-22 15:46:32 -0500

    fix style button

--- src/Components/StatusLine.jsx
+++ src/Components/StatusLine.jsx
@@ -73,7 +73,7 @@ export default function StatusLine(props) {
               +
             </button>
 
-            <button onClick={randomTask} className="button">
+            <button onClick={randomTask} className="button randomTask">
               random tasks
             </button>
           </div>      
